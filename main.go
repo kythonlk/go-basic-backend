7a757184da567806fdcc0e4efd2e67909c560dcd
kyhtonllk <kythonlk@gmail.com>
2024-02-27 11:50:40 +0400

    update

--- main.go
+++ main.go
@@ -1,24 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
-import "fmt"
-import "net/http"
+import (
+  "go-cms/router"
+)
 
 func main() {
-  fmt.Println("app running in http://0.0.0.0:8080")
-	r := gin.Default()
-  
-  r.LoadHTMLGlob("src/ui/*.templ")
 
-r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "home.templ", gin.H{
-      "title": "Home",
-    })
-	})
+  r := router.Router()
 
-	r.GET("/about", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "about.templ", gin.H{})
-	})
-
-	r.Run(":8000") // listen and serve on 0.0.0.0:8080
+	r.Run(":8000") // Listen and serve on 0.0.0.0:8080
 }
